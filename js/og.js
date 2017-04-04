78251fea0c0b96a533d05693becd1c2991631c14
Clayton Nummer <nummercl@wblib.org>
2017-04-04 11:55:08 -0400

    Don't cut bnumber check digit
    because we are no longer sending it

--- js/og.js
+++ js/og.js
@@ -78,9 +78,6 @@ $('#og-list').empty();
 			$.each(books_json, function( index, value ) {
 
 
-			var bib_record_num = value['bib_record_num'].substring(0,value['bib_record_num'].length - 1)
-
-
 				var img = new Image();
 				img.onload = function() {
   					if (this.width > 1) {
