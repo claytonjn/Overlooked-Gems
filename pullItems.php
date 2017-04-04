1e025250d174ca8e30b1acb81c4de82e3d2d3bfd
Clayton Nummer <claytonjn@gmail.com>
2017-04-03 21:51:52 -0400

    Clean up JSON

--- pullItems.php
+++ pullItems.php
@@ -150,7 +150,7 @@
 
     $sierraResult = pg_query($sierraDNAconn, $pullQuery) or die('Query failed: ' . pg_last_error());
 
-    $json = "{\"things\":{\"thing\":[";
+    $json = "[";
 
     while ($row = pg_fetch_assoc($sierraResult)) {
         $row['bib_record_num'] = "b" . $row['bib_record_num'] . getCheckDigit($row['bib_record_num']);
@@ -162,7 +162,7 @@
 
     $json = substr($json, 0, -1);
 
-    $json .= "]}}";
+    $json .= "]";
 
     echo $json;
 
