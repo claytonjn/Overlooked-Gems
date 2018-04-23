667c4fbbc701c578bf2168e19a23f9e0eab15863
Clayton Nummer <nummercl@wblib.org>
2018-04-23 19:07:41 -0400

    Look at suffled reading history one at a time
    and look for recommendations until we find one that's available

--- getRecommendations.php
+++ getRecommendations.php
@@ -12,18 +12,37 @@
 	//Connect to SierraDNA
 	$sierraDNAconn = db_sierradna();
 
+	$pnumber = '1051149';
+
     $zolaSignature = zolaSignature();
-	$readISBN = pullReadingHistory('1124032', $sierraDNAconn);
+	$readISBNS = pullReadingHistory($pnumber, $sierraDNAconn);
+	shuffle($readISBNS);
 
-    $zolaRecommendations =  zolaRecommendations($zolaSignature, $readISBN, NULL, NULL, "BB,BC,BH,WW,BK,AC", "TRUE");
-    $recommendations = json_decode($zolaRecommendations, true)['data']['list'];
+	foreach($readISBNS as $isbn) {
+		$zolaRecommendations = zolaRecommendations($zolaSignature, $isbn, 100, NULL, "BB,BC,BH,WW,BK,AC", "TRUE");
+		$recommendations = json_decode($zolaRecommendations, true);
+		if($recommendations['status'] == "success") {
+			$recommendations = $recommendations['data']['list'];
 
-    $recommendedISBNS = array();
-    foreach ($recommendations as $recommendation) {
-        foreach($recommendation['versions'] as $versions) {
-            array_push($recommendedISBNS, $versions['isbn']);
-        }
-    }
+			$recommendedISBNS = array();
+		    foreach($recommendations as $recommendation) {
+		        foreach($recommendation['versions'] as $versions) {
+					if(in_array($versions['raw_form'], ["BB", "BC", "BH", "WW", "BK", "AC"])) {
+						array_push($recommendedISBNS, $versions['isbn']);
+					}
+		        }
+		    }
 
-	echo checkSierraForHit($recommendedISBNS, $sierraDNAconn);
+			shuffle($recommendedISBNS);
+			$sierraResult = checkSierraForHit($recommendedISBNS, $pnumber, $sierraDNAconn);
+
+			if(pg_num_rows($sierraResult) > 0) {
+				while ($row = pg_fetch_assoc($sierraResult)) {
+					echo $row['record_id']."<br>";
+				}
+				exit; //got a result, don't keep trying
+			}
+		}
+	}
+
 ?>
--- includes/functions.php
+++ includes/functions.php
@@ -27,7 +27,7 @@
 	}
 
 	function pullReadingHistory($pnumber, $sierraDNAconn) {
-		//Create temp table with unpagable holds filtered out at the bib level
+		//Pull books from patron's reading history
 		$query = "	SELECT		(	SELECT 		DISTINCT ON (v.record_id) v.field_content
 									FROM 		sierra_view.varfield v
 									WHERE 		v.record_id = RH.bib_record_metadata_id
@@ -44,17 +44,74 @@
 								OR	brp.material_code = 'i'	/*	Book on Tape (15)	*/
 								OR	brp.material_code = 'p'	/*	Book on MP3 (23)	*/
 								OR	brp.material_code = 'z'	/*	eBook (24)			*/
-								OR	brp.material_code = '$'	/*	Rental (25)			*/	)
-					ORDER BY	RH.checkout_gmt DESC
-					LIMIT		100;";
+								OR	brp.material_code = '$'	/*	Rental (25)			*/	);";
 
 		$sierraResult = pg_query($sierraDNAconn, $query) or die('Query failed: ' . pg_last_error());
 
-		$readISBNS = "";
+		$readISBNS = [];
 		while($row=pg_fetch_assoc($sierraResult)) {
-			$readISBNS .= cleanFromSierra("ident", $row['ident']).",";
+			array_push($readISBNS, cleanFromSierra("ident", $row['ident']));
 		}
-		return rtrim($readISBNS,",");
+		return $readISBNS;
+	}
+
+	function checkSierraForHit($recommendedISBNS, $pnumber, $sierraDNAconn) {
+
+		$query =	"	DROP TABLE IF EXISTS varfields;";
+		$query .=	"	CREATE TEMP TABLE varfields
+						(
+							record_id bigint,
+							field_content varchar(20001)
+						);";
+		$query .=	"	INSERT INTO	varfields
+						SELECT 		record_id, field_content
+						FROM		sierra_view.varfield
+						WHERE		marc_tag = '020'
+						AND			varfield_type_code = 'i';";
+		$query .=	"	DROP TABLE IF EXISTS bibRecords;";
+		$query .=	"	DROP TABLE IF EXISTS reading_histories;";
+		$query .=	"	CREATE TEMP TABLE reading_histories
+						(
+							bib_record_metadata_id bigint
+						);";
+		$query .=	"	INSERT INTO reading_histories
+						SELECT		bib_record_metadata_id
+						FROM		sierra_view.reading_history rh
+						LEFT JOIN	sierra_view.patron_view pv
+									ON rh.patron_record_metadata_id = pv.id
+						WHERE		pv.record_num = '{$pnumber}';";
+		$query .=	"	CREATE TEMP TABLE bibRecords
+						(
+							record_id bigint
+						);";
+		$query .=	"	INSERT INTO bibRecords
+						SELECT		v.record_id
+						FROM		varfields v
+						LEFT JOIN	reading_histories rh
+									ON v.record_id = rh.bib_record_metadata_id
+						LEFT JOIN	sierra_view.bib_record_property brp
+									ON v.record_id = brp.bib_record_id
+						LEFT JOIN	sierra_view.bib_record_item_record_link brirl
+									ON v.record_id = brirl.bib_record_id
+						LEFT JOIN	sierra_view.item_view iv
+									ON brirl.item_record_id = iv.id
+						LEFT JOIN	sierra_view.checkout c
+									ON brirl.item_record_id = c.item_record_id
+						WHERE		rh.bib_record_metadata_id IS NULL
+						AND			brp.material_code = 'a'	/*	Book (7)	*/
+						AND			iv.item_status_code = '-'
+						AND			c.item_record_id IS NULL
+						AND			";
+		$query .=	"(";
+		foreach($recommendedISBNS as $isbn) {
+			$query .= "v.field_content LIKE '%{$isbn}%' OR ";
+		}
+		$query = rtrim($query," OR ") . ")";
+		$query .=	"	GROUP BY	v.record_id;";
+		$query .= "	SELECT * FROM bibRecords";
+
+		$sierraResult = pg_query($sierraDNAconn, $query) or die('Query failed: ' . pg_last_error());
+		return $sierraResult;
 	}
 
 	function zolaSignature() {
