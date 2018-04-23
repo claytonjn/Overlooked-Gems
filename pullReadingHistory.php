3fbf2e36d138073429f5de3414c55992416c6cc6
Clayton Nummer <nummercl@wblib.org>
2018-04-23 12:56:16 -0400

    Auto stash before merge of "master" and "origin/master"

--- pullReadingHistory.php
+++ pullReadingHistory.php
@@ -11,30 +11,28 @@
 
 	//Connect to SierraDNA
 	$sierraDNAconn = db_sierradna();
-	
-	
-		//Create temp table with unpagable holds filtered out at the bib level
-	$query = "	SELECT		(SELECT 	DISTINCT ON (v.record_id) v.field_content
-							FROM 		sierra_view.varfield v
-							WHERE 		v.record_id = RH.bib_record_metadata_id
-							AND		v.marc_tag = '020'
-							ORDER BY 	v.record_id, v.marc_tag DESC, v.occ_num ASC) AS ident
+
+	//Create temp table with unpagable holds filtered out at the bib level
+	$query = "	SELECT		(	SELECT 		DISTINCT ON (v.record_id) v.field_content
+								FROM 		sierra_view.varfield v
+								WHERE 		v.record_id = RH.bib_record_metadata_id
+								AND			v.marc_tag = '020'
+								ORDER BY	v.record_id, v.marc_tag DESC, v.occ_num ASC	) AS ident
 				FROM		sierra_view.reading_history AS RH
 				LEFT JOIN	sierra_view.patron_view ON rh.patron_record_metadata_id = patron_view.id
 				LEFT JOIN	sierra_view.bib_record_property AS BRP ON RH.bib_record_metadata_id = BRP.bib_record_id
 				WHERE		patron_view.record_num = '1124032'
-				AND		(	   brp.material_code = 'b'	/*	Book on CD (5)		*/
-							OR brp.material_code = 'a'	/*	Book (7)		*/
-							OR brp.material_code = 'l'	/*	Large Print (10)	*/
-							OR brp.material_code = 'k' 	/*	eAudio (13)		*/
-							OR brp.material_code = 'i'	/*	Book on Tape (15)	*/
-							OR brp.material_code = 'p'	/*	Book on MP3 (23)	*/
-							OR brp.material_code = 'z'	/*	eBook (24)		*/
-							OR brp.material_code = '$'	/*	Rental (25)		*/ )";
-							
+				AND			(	brp.material_code = 'b'	/*	Book on CD (5)		*/
+							OR	brp.material_code = 'a'	/*	Book (7)			*/
+							OR	brp.material_code = 'l'	/*	Large Print (10)	*/
+							OR	brp.material_code = 'k' /*	eAudio (13)			*/
+							OR	brp.material_code = 'i'	/*	Book on Tape (15)	*/
+							OR	brp.material_code = 'p'	/*	Book on MP3 (23)	*/
+							OR	brp.material_code = 'z'	/*	eBook (24)			*/
+							OR	brp.material_code = '$'	/*	Rental (25)			*/	)";
+
 	$sierraResult = pg_query($sierraDNAconn, $query) or die('Query failed: ' . pg_last_error());
-							
-							
+
 	while($row=pg_fetch_assoc($sierraResult))
 	{
 		$isbnString=cleanFromSierra("ident", $row['ident']).",";
