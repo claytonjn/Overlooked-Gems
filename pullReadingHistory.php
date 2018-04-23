e9ca64a02f9faacff8e20482c7a9fef8a8ad7dc4
Unknown <pesalero@wblib.org>
2018-04-23 12:43:09 -0400

    Echoed out ISBN Data

--- pullReadingHistory.php
+++ pullReadingHistory.php
@@ -18,7 +18,7 @@
 							FROM 		sierra_view.varfield v
 							WHERE 		v.record_id = RH.bib_record_metadata_id
 							AND		v.marc_tag = '020'
-							ORDER BY 	v.record_id, v.marc_tag DESC, v.occ_num ASC) AS IDENT
+							ORDER BY 	v.record_id, v.marc_tag DESC, v.occ_num ASC) AS ident
 				FROM		sierra_view.reading_history AS RH
 				LEFT JOIN	sierra_view.patron_view ON rh.patron_record_metadata_id = patron_view.id
 				LEFT JOIN	sierra_view.bib_record_property AS BRP ON RH.bib_record_metadata_id = BRP.bib_record_id
@@ -32,7 +32,10 @@
 							OR brp.material_code = 'z'	/*	eBook (24)		*/
 							OR brp.material_code = '$'	/*	Rental (25)		*/ )";
 							
+	$sierraResult = pg_query($sierraDNAconn, $query) or die('Query failed: ' . pg_last_error());
 							
 							
-							
-?>
+	while($row=pg_fetch_assoc($sierraResult))
+	{
+		echo cleanFromSierra("ident", $row['ident'])."<br>";
+	}
