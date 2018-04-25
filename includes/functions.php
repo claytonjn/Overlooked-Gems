bbfebff89cf6a82b1f0ddc62fbf6086cfa53be30
Unknown <pesalero@wblib.org>
2018-04-25 13:18:36 -0400

    Updated Clean Sierra function to use author instead of Best_Author

--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,7 @@
 
 	function cleanFromSierra($field, $string) {
 		switch ($field) {
-			case "best_author":
+			case "author":
 				$pattern = "/([^\d,\.]*,?[^\d,\.]*)(?:,|\.)?.*/";
 				$replacement = "$1";
 				break;
