e87ecd894b45ff902c4e1f4a8282c16df47b83fd
Unknown <pesalero@wblib.org>
2018-04-25 22:33:45 -0400

    Created a header file and incorporated it into index and myaccount

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
 	//**This include file includes functions, such as pipe cleaner
 	include_once ("./includes/functions.php");
 
+	//**This include file includes header
+	include_once ("./includes/header.php");
+
 	//Connect to SierraDNA
 	$sierraDNAconn = db_sierradna();
 	
@@ -59,11 +62,11 @@
 	
 	echo <<< Text
 		
-		<div id="header">
+		<div id="headertext">
 			Sign-up Form
 		</div>
 		
-		<div id="FormInfo">
+		<div id="forminfo">
 			<span style="color:#C00;">{$errorDisplay}</span>
 			<form action="./process_login.php" method="POST" name="SIGNUP">
 				<div id="textbox">
@@ -82,128 +85,5 @@
 		
 		
 Text;
-	
-	
-	
-	die;
-	/*
-	else if($step==2) {
-		
-		$patronQuery = "SELECT 		PRF.last_name, PV.record_num, PV.id
-						FROM 		sierra_view.varfield AS V
-						LEFT JOIN	sierra_view.patron_view AS PV ON PV.id = V.record_id
-						LEFT JOIN	sierra_view.patron_record_fullname AS PRF ON PRF.patron_record_id = V.record_id
-						WHERE 		V.field_content = '$barcode';";
-
-		$sierraPatronResult = pg_query($sierraDNAconn, $patronQuery) or die('Query failed: ' . pg_last_error());
-		
-		$resultCount =  pg_num_rows($sierraPatronResult);
-		
-		if($resultCount == 0) {
-			$error = 1;
-			header("Location index.php?error=$error");
-			die;
-		}
-		elseif($resultCount == 1) {	
-
-			//Get the Single Row of Information
-			$row = pg_fetch_assoc($sierraPatronResult);
-			
-			//Grab the lastname from the PG Query
-			$pgLastname = strtolower(str_replace( "*", "", $row['last_name'] ));
-			
-			//Set the Pnumber from the PG Query
-			$pnumber = $row['record_num'];
-
-			//Set Session Variables
-			$_SESSION['PNumber']=$pnumber;
-			
-			//Compare names to see if login is valid
-			if($pgLastname != $lastname)				
-			{
-				//NOT FOUND
-				$error = 1;
-				header("Location: index.php?error=$error&lastname=$lastname&barcode=$barcode");
-				exit;
-			}
-			else if($pgLastname == $lastname) {		
-
-				$patronRHQuery = "	SELECT 		is_reading_history_opt_in AS rhbool
-									FROM 		sierra_view.patron_record AS PR
-									WHERE 		PR.record_id = '{$row['id']}';";
-
-				$sierraPatronRHResult = pg_query($sierraDNAconn, $patronRHQuery) or die('Query failed: ' . pg_last_error());
-				
-				$rhRow = pg_fetch_assoc($sierraPatronRHResult);
-				
-				if($rhRow['rhbool']=='f') {
-					echo <<< NoReadingHistory
-						In order to use this service, you must have your reading history turned on.<br>
-						To do this, please login to "My Account", select Reading History and "Opt In".		
-NoReadingHistory;
-				}
-				elseif($rhRow['rhbool']=='t') {
-					echo <<< Frequency
-						<div id="FormInfo">
-							<form action="" method="POST" name="FrequencyForm">
-								<input type="hidden" value="3" name="step"
-								<div id="Select">
-									Pick your hold Frequency:
-									<select name="frequency">
-										<option value='weekly'>Weekly</option>
-										<option value='biweekly'>Bi-weekly</option>
-										<option value='monthly' SELECTED>Monthly</option>
-										<option value='bimonthly'>Bi-monthly</option>
-										<option value='quarterly'>Quarterly</option>
-										<option value='biannually'>Bi-annually</option>
-										<option value='annually'>Annually</option>
-									</select>
-								</div>
-								<div id="Select">
-									Select your pick up location:
-									<select name="pickup_location">
-										<option value='drive'>Main Drive-up</option>
-										<option value='west' SELECTED>Main Lobby</option>
-										<option value='wacr'>Westacres</option>
-									</select>
-								</div>
-								<div id="textbox">
-									<input type="submit" name="submit">
-								</div>
-							</form>
-						</div>
-Frequency;
-				}
-			}
-		}
-	}
-	else if($step==3) {
-		
-		if(isset($_POST['frequency']))
-			$frequency = $_POST['frequency'];
-		else
-			$frequency = "monthly";
-		
-		if(isset($_POST['pickup_location']))
-			$pickup_location = $_POST['pickup_location'];
-		else
-			$pickup_location = "west";
-		
-		date_default_timezone_set('America/Detroit');
-		$next_trigger=date("Y-m-d",strtotime("tomorrow"));
-		
-		
-		$query = "	INSERT INTO 2018_patrons (pnumber, pickup_location, frequency, next_trigger)
-					 VALUES ('{$_SESSION['PNumber']}', '{$pickup_location}', '{$frequency}', '{$next_trigger}')
-					 ON DUPLICATE KEY UPDATE
-					 pickup_location	= '{$pickup_location}',
-					 frequency 			= '{$frequency}',
-					 next_trigger		= '{$next_trigger}';";
-		
-		$result = mysqli_query($overlookedGemsLink, $query) or die(mysqli_error($overlookedGemsLink));
-		
-		echo $next_trigger;
-	}
-	*/
 	
 ?>
--- myaccount.php
+++ myaccount.php
@@ -109,33 +109,14 @@
 			case '-1':
 				$filter_text = 'Thumbs Down';
 				break;
-		}			
+		}
+
+		//**This include file includes header
+		include_once ("./includes/header.php");
+	
 		echo <<< HTML
-		  <!doctype html>
-		  <html lang="en">
-		  <head>
-			<meta charset="utf-8">
-			<title>Overlooked Gems</title>
-		 
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		 
-			<!--[if lt IE 9]>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-			<![endif]-->
-		 
-			<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-			<link href="//fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
-			<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-			<link href="//cdn.materialdesignicons.com/2.3.54/css/materialdesignicons.min.css" rel="stylesheet">
-			<link rel="stylesheet" href="css/og.css?v=1.8">
-		 
-		 
-			<script src="//code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-		 
-		  </head>
-		 
-		  <body>
-		 
+
+		
 		 
 		  <header>
 		  <div id="patnum" type="hidden" data-val="{$pnumber}">
