5100782c3f95a0c18569b6c21ee35379241d0a90
Clayton Nummer <nummercl@wblib.org>
2018-04-25 20:59:30 -0400

    Add script for rating titles

---
+++ rateTitle.php
@@ -0,0 +1,38 @@
+<?php
+
+	ini_set ("display_errors", "1");
+	error_reporting(E_ALL);
+
+    if(!isset($_GET['pnumber'])) {
+        echo "No patron information was sent - title can't be rated!";
+    } else {
+        $pnumber = $_GET['pnumber'];
+    }
+    if(!isset($_GET['bib_record_metadata_id'])) {
+        echo "No title information was sent - title can't be rated!";
+    } else {
+        $bib_record_metadata_id = $_GET['bib_record_metadata_id'];
+    }
+    if(!isset($_GET['rating'])) {
+        echo "No rating information was sent - title can't be rated!";
+    } else {
+        $rating = $_GET['rating'];
+    }
+
+	//Database connections
+	include_once ("./includes/db_connect.inc");
+
+    //Connect to the overlooked_gems database
+	$overlookedGemsLink = db_overlooked_gems() or die ("Cannot connect to server");
+
+    $rateQuery = "  UPDATE  `2018_reading_history`
+                    SET     rating = '{$rating}'
+                    WHERE   pnumber = {$pnumber}
+                    AND     bib_record_metadata_id = {$bib_record_metadata_id};";
+
+    echo mysqli_query($overlookedGemsLink, $rateQuery) or die(mysqli_error($overlookedGemsLink));
+
+	mysqli_close($overlookedGemsLink);
+	unset($overlookedGemsLink);
+
+?>
